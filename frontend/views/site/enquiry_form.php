e9987300023d3d57297c3d772b712814b34b5779
Yogesh Harke <72489809+yogeshharke@users.noreply.github.com>
2024-05-06 20:37:02 +0530

    Vacation Type select2 changes in enquiry form (#8)
    * TourLandingImage changes
    
    Tour Landing Image and packages-page page dynamic changes.
    
    * Tour enquiry changes
    
    Tour enquiry changes
    
    * Enquiry button fix position and form split in 2 section
    
    Enquiry button fix position and form split in 2 section
    
    * Moved inquiry button near the footer
    
    Moved inquiry button near the footer and validated form
    
    * Squashed commit of the following:
    
    commit fc479e255d85237c83e0f0514da341905ffbbc18
    Author: sadafulevaibhav <sadafulevaibhav@gmail.com>
    Date:   Sat May 4 20:01:15 2024 +0530
    
        Update main.php
    
    commit 95d5bdfcb11908a390229265aa1a17400c9cd2f6
    Author: sadafulevaibhav <sadafulevaibhav@gmail.com>
    Date:   Sat May 4 19:39:34 2024 +0530
    
        Update index.php
    
    commit 3354c5fa3c59ca1b4622d90ac22943535ade3a03
    Author: sadafulevaibhav <sadafulevaibhav@gmail.com>
    Date:   Sat May 4 19:33:37 2024 +0530
    
        Update packages-page.php
    
    commit 510cc507835c759de2b1a1dc0ba8a9320705a206
    Author: sadafulevaibhav <sadafulevaibhav@gmail.com>
    Date:   Sat May 4 19:18:50 2024 +0530
    
        style fix
    
    commit 903b67b15670ef351589c6a5340352159907fb79
    Author: sadafulevaibhav <sadafulevaibhav@gmail.com>
    Date:   Sat May 4 18:53:54 2024 +0530
    
        design fix
    
    commit 80c66085e0ead5868847016694430207bb1206d0
    Author: sadafulevaibhav <sadafulevaibhav@gmail.com>
    Date:   Fri May 3 01:38:09 2024 +0530
    
        Update main.php
    
    commit c9b74bfebfbe3e9cf92ddb8160ec503e4e2ad864
    Author: sadafulevaibhav <sadafulevaibhav@gmail.com>
    Date:   Fri May 3 01:32:31 2024 +0530
    
        Update main.php
    
    commit 50eae65e29e13f318919ae2d317149059a04e818
    Author: sadafulevaibhav <sadafulevaibhav@gmail.com>
    Date:   Fri May 3 00:58:08 2024 +0530
    
        modal close button fix
    
    commit 09e4ba9467ff4c12bcc75ba7a5811f6da619d7f8
    Merge: 69dd346 072e88e
    Author: sadafulevaibhav <sadafulevaibhav@gmail.com>
    Date:   Fri May 3 00:37:12 2024 +0530
    
        Merge branch 'main' of https://github.com/sadafulevaibhav/pushpakvimana
    
    commit 69dd346ed9c7849813935f7c3317afc46ea59947
    Author: sadafulevaibhav <sadafulevaibhav@gmail.com>
    Date:   Fri May 3 00:37:01 2024 +0530
    
        Update SiteController.php
    
    commit 072e88e3933e8d36ee56df2e4826e0af435127c5
    Author: Yogesh Harke <72489809+yogeshharke@users.noreply.github.com>
    Date:   Fri May 3 00:36:16 2024 +0530
    
        Moved inquiry button in the footer and shown above footer and validated form (#7)
    
        * TourLandingImage changes
    
        Tour Landing Image and packages-page page dynamic changes.
    
        * Tour enquiry changes
    
        Tour enquiry changes
    
        * Enquiry button fix position and form split in 2 section
    
        Enquiry button fix position and form split in 2 section
    
        * Moved inquiry button near the footer
    
        Moved inquiry button near the footer and validated form
    
        ---------
    
        Co-authored-by: sadafulevaibhav <sadafulevaibhav@gmail.com>
    
    * Vacation Type changes in enquiry form
    
    Vacation Type select2 changes in enquiry form
    
    ---------
    
    Co-authored-by: sadafulevaibhav <sadafulevaibhav@gmail.com>

--- frontend/views/site/enquiry_form.php
+++ frontend/views/site/enquiry_form.php
@@ -2,7 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use common\models\GeneralPicklist;
 use kartik\date\DatePicker;
+use kartik\select2\Select2;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\TourEnquiries */
@@ -18,15 +20,15 @@ use kartik\date\DatePicker;
     <div class="row">
         <div class="col-md-12 enq-section-1">
             <!-- Content for Section 1 -->
-            <?= $form->field($model, 'full_name')->textInput(['maxlength' => true,'placeholder' => "Full Name"])->label(false) ?>
-            <?= $form->field($model, 'email')->textInput(['maxlength' => true,'placeholder' => "Email"])->label(false) ?>
-            <?= $form->field($model, 'mobile')->textInput(['maxlength' => true,'placeholder' => "Phone Number"])->label(false) ?>
-            <?= $form->field($model, 'whats_app')->textInput(['maxlength' => true,'placeholder' => "WhatsApp"])->label(false) ?>
+            <?= $form->field($model, 'full_name')->textInput(['maxlength' => true, 'placeholder' => "Full Name"])->label(false) ?>
+            <?= $form->field($model, 'email')->textInput(['maxlength' => true, 'placeholder' => "Email"])->label(false) ?>
+            <?= $form->field($model, 'mobile')->textInput(['maxlength' => true, 'placeholder' => "Phone Number"])->label(false) ?>
+            <?= $form->field($model, 'whats_app')->textInput(['maxlength' => true, 'placeholder' => "WhatsApp"])->label(false) ?>
         </div>
 
         <div class="col-md-12 enq-section-2">
             <!-- Content for Section 2 -->
-            <?= $form->field($model, 'travel_destination')->textInput(['maxlength' => true,'placeholder' => "Travel Destination"])->label(false)?>
+            <?= $form->field($model, 'travel_destination')->textInput(['maxlength' => true, 'placeholder' => "Travel Destination"])->label(false) ?>
             <?= $form->field($model, 'travel_date')->widget(DatePicker::classname(), [
                 'options' => ['placeholder' => 'Travel Date'],
                 'pluginOptions' => [
@@ -34,8 +36,22 @@ use kartik\date\DatePicker;
                     'format' => 'yyyy-mm-dd'
                 ]
             ])->label(false); ?>
-            <?= $form->field($model, 'travellers_count')->textInput(['type' => 'number','placeholder' => "No. of people"])->label(false) ?>
-            <?= $form->field($model, 'vaccation_type')->textInput(['maxlength' => true, 'placeholder' => "Vacation Type"])->label(false) ?>
+            <?= $form->field($model, 'travellers_count')->textInput(['type' => 'number', 'placeholder' => "No. of people"])->label(false) ?>
+            <!--
+            <?= $form->field($model, 'vacation_type')->textInput(['maxlength' => true, 'placeholder' => "Vacation Type"])->label(false) ?>
+            -->
+            <?= $form->field($model, 'vacation_type')->widget(Select2::classname(), [
+                'data' => GeneralPicklist::find()
+                    ->select(['name', 'id'])
+                    ->where(['category' => 'Vacation Type'])
+                    ->indexBy('id')
+                    ->column(),
+                'options' => ['placeholder' => 'Select Vacation Type'],
+                'pluginOptions' => [
+                    'allowClear' => true
+                ],
+            ]); ?>
+
         </div>
     </div>
 
